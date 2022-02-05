9cb8e671f34a8a19dc62c201db38e23504d5ae0b
rldhks8745 <sdaff1004@gmail.com>
2022-02-05 22:37:53 +0900

    🎨 Update : StringTest 테스트케이스 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,46 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] splitedString = "1,2".split(",");
+        assertThat(splitedString).contains("1", "2");
+
+        splitedString = "1".split(",");
+        assertThat(splitedString).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String str = "(1,2)";
+        str = str.substring(0, str.length() - 1);
+        str = str.substring(1);
+
+        assertThat(str).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("StringIndexOutOfBoundsException 테스트")
+    void charAt() {
+        // given
+
+        // when
+        Throwable thrown = catchThrowable(() -> "abc".charAt(3));
+
+        // then
+        assertThat(thrown).isInstanceOf(StringIndexOutOfBoundsException.class)
+                        .hasMessageContaining("String index out of range: 3");
     }
 }
